0582318edd687bcd855d10962699db1c18f0ee94
Arun Raghavan <git@arunraghavan.net>
2015-05-25 16:07:09 +0530

    Add email address in copyright notice

--- src/TinyXML.hs
+++ src/TinyXML.hs
@@ -1,5 +1,5 @@
 {-
-Copyright 2015 Arun Raghavan
+Copyright 2015 Arun Raghavan <mail@arunraghavan.net>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
