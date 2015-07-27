e132ba363be123b7b8087eeec3d8f415731bea28
Arun Raghavan <git@arunraghavan.net>
2015-07-27 19:15:37 +0530

    Remove unused language extension

--- src/XML2UCM.hs
+++ src/XML2UCM.hs
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 -}
 
-{-# LANGUAGE RecordWildCards, DisambiguateRecordFields #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module XML2UCM
 ( xml2ucm
